86ac5cf87bccb81dd2e5d6ac3d25487a056708d5
Vita Fomin <vita.bukhalo@gmail.com>
2023-03-08 15:28:40 -0500

    added arrays to my variables

--- script.js
+++ script.js
@@ -14,16 +14,25 @@ function generatePassword() {
 
   prompt("What is your desired password length?")
 
-  var charTypeLow 
+  if ("passLength < 8 || passLength > 128") {
+    alert("Your password must be between 8 and 128 characters");
+    prompt("What is your desired password length?");
+  }
+
+  var charTypeLow = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];  
+
   confirm("Would you like to include lowercase letters?")
 
-  var charTypeUp
+  var charTypeUp = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; 
+
   confirm("Would you like to include uppercase letters?")
 
-  var charTypeNum
+  var charTypeNum = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]; 
+
   confirm("Would you like to include any numbers?")
 
-  var charTypeSpec
+  var charTypeSpec = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "}", "|", "?", "/"]; 
+
   confirm("Would you like to include any special characters?")  
 
 }
