655ff3c4f5591cd291939e674acfdca075b6d60c
Vita Fomin <vita.bukhalo@gmail.com>
2023-03-08 14:47:55 -0500

    created a function section for the generatePassword function

--- script.js
+++ script.js
@@ -7,8 +7,29 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
+function generatePassword() {
+
+  var passLength
+
+  prompt("What is your desired password length?")
+
+  var charTypeLow 
+  confirm("Would you like to include lowercase letters?")
+
+  var charTypeUp
+  confirm("Would you like to include uppercase letters?")
+
+  var charTypeNum
+  confirm("Would you like to include any numbers?")
+
+  var charTypeSpec
+  confirm("Would you like to include any special characters?")  
 
 }
 
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
