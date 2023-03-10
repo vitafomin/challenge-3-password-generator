807891f20873ed33225551f0f708655a5fc35c13
Vita Fomin <vita.bukhalo@gmail.com>
2023-03-10 00:11:51 -0500

    got all prompts to display and combined all arrays of the code

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // Assignment Code
+console.log(password)
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -17,25 +19,25 @@ function generatePassword() {
     prompt("What is your desired password length?");
   }
   
-
   var charTypeLow = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];  
 
-  confirm("Would you like to include lowercase letters?")
+  // confirm("Would you like to include lowercase letters?")
 
   var charTypeUp = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; 
 
-  confirm("Would you like to include uppercase letters?")
+  // confirm("Would you like to include uppercase letters?")
 
   var charTypeNum = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]; 
 
-  confirm("Would you like to include any numbers?")
+  // confirm("Would you like to include any numbers?")
 
   var charTypeSpec = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "}", "|", "?", "/"]; 
 
-  confirm("Would you like to include any special characters?")  
+  // confirm("Would you like to include any special characters?")  
 
-  var combineArray = []
-
+  var combineArray = [];
+  
+  
   if (confirm("Would you like to include lowercase letters?")){
     combineArray = combineArray.concat(charTypeLow);
   }
@@ -48,17 +50,21 @@ function generatePassword() {
   if (confirm("Would you like to include any special characters?")){
     combineArray = combineArray.concat(charTypeSpec);
   }
+  console.log(combineArray);
  
-  // for (var i = 0; i < passLength; i++) {
-  //   var randomPass = math.floor(math.random() * combineArray.length);
-  //   pass = pass + combineArray[randomPass];
-  // }
+  var newPassword; 
+  for (var i = 0; i < passLength; i++) {
+    var randomPass = combineArray[Math.floor(Math.random() * combineArray.length)];
+    
+    newPassword = combineArray[randomPass];
+  }
+  console.log(newPassword)
+  return newPassword;
+ 
+
 }
-
-
-
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// Getting undefined at the moment, need to debug for loop
