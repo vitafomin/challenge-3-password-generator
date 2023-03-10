ede71cf1061c1ddf5f60df528d2ae14df35af4d2
Vita Fomin <vita.bukhalo@gmail.com>
2023-03-10 12:40:18 -0500

    worked on for loop in seperate steps, added comments to help keep track of my progress

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Assignment Code
-console.log(password)
+console.log(password);
 
 var generateBtn = document.querySelector("#generate");
 
@@ -11,60 +11,173 @@ function writePassword() {
   passwordText.value = password;
 }
 function generatePassword() {
-
-  var passLength = prompt("What is your desired password length?")
+  var passLength = prompt("What is your desired password length?");
 
   if (passLength < 8 || passLength > 128) {
     alert("Your password must be between 8 and 128 characters");
     prompt("What is your desired password length?");
   }
-  
-  var charTypeLow = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];  
 
-  // confirm("Would you like to include lowercase letters?")
+   // confirm("Would you like to include lowercase letters?")
 
-  var charTypeUp = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; 
+  var charTypeLow = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
 
-  // confirm("Would you like to include uppercase letters?")
+// confirm("Would you like to include uppercase letters?")
 
-  var charTypeNum = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]; 
+  var charTypeUp = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
 
+ 
   // confirm("Would you like to include any numbers?")
 
-  var charTypeSpec = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "}", "|", "?", "/"]; 
+  var charTypeNum = [
+    "0", 
+    "1", 
+    "2", 
+    "3", 
+    "4", 
+    "5", 
+    "6", 
+    "7", 
+    "8", 
+    "9"
+  ];
 
-  // confirm("Would you like to include any special characters?")  
+  // confirm("Would you like to include any special characters?")
+
+  var charTypeSpec = [
+    "~",
+    "`",
+    "!",
+    "@",
+    "#",
+    "$",
+    "%",
+    "^",
+    "&",
+    "*",
+    "(",
+    ")",
+    "_",
+    "-",
+    "+",
+    "=",
+    "{",
+    "}",
+    "|",
+    "?",
+    "/",
+  ];
+
 
   var combineArray = [];
-  
-  
-  if (confirm("Would you like to include lowercase letters?")){
+
+  if (confirm("Would you like to include lowercase letters?")) {
     combineArray = combineArray.concat(charTypeLow);
   }
-  if (confirm("Would you like to include uppercase letters?")){
+
+  if (confirm("Would you like to include uppercase letters?")) {
     combineArray = combineArray.concat(charTypeUp);
   }
-  if (confirm("Would you like to include any numbers?")){
+
+  if (confirm("Would you like to include any numbers?")) {
     combineArray = combineArray.concat(charTypeNum);
   }
-  if (confirm("Would you like to include any special characters?")){
+
+  if (confirm("Would you like to include any special characters?")) {
     combineArray = combineArray.concat(charTypeSpec);
   }
-  console.log(combineArray);
- 
-  var newPassword; 
-  for (var i = 0; i < passLength; i++) {
-    var randomPass = combineArray[Math.floor(Math.random() * combineArray.length)];
-    
-    newPassword = combineArray[randomPass];
-  }
-  console.log(newPassword)
-  return newPassword;
- 
 
+  console.log("Avaiable Characters: ", combineArray);
+
+  // newPassword is a temp variable to hold data, this will be a STRING
+  var newPassword = "";
+
+  for (var i = 0; i < passLength; i++) {
+
+    // this returns A number between 0 and 1, but does not include 1.
+    var rando = Math.random() * combineArray.length;
+
+    // apply (Math.floor method to rando) and, this returns a WHOLE number of the number of things in the Array (0 - number of things in array)
+    var index = Math.floor(rando); 
+    
+    // this will give me the VALUE that is stored in the Array at a specific INDEX
+    var randomCharacter = combineArray[index]; 
+    
+
+    // here we are grabbing the VALUE at INDEX in the ARRAY
+    console.log("Random Num: ", rando);
+    console.log("Index: ", index);
+    console.log("Char: ", randomCharacter);
+
+
+    // Here we are ADDING to our temp dataset (PASSWORD)
+
+
+    newPassword = newPassword + randomCharacter;
+    
+    console.log("Password: ", newPassword);
+  }
+
+  console.log(newPassword);
+  return newPassword;
+  
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Getting undefined at the moment, need to debug for loop
+
